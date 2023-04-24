b6b8c2792e37f63e9fa88fa9704cc5b6bcbada33
Andrés Agosto <2981747582009@ingenieria.usac.edu.gt>
2023-04-23 23:04:02 -0600

    usamos cors

--- server/servidor.py
+++ server/servidor.py
@@ -1,8 +1,11 @@
 #iniciamos el proyecto
 
 from flask import Flask
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app) #restringimos y aseguramos solicitudes http externas
 
 @app.route('/')
 
